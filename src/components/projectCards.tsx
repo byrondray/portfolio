fd182651e5f9a5585ad52218b90d957ab6ef595b
byron dray <byrondray8@gmail.com>
2025-02-19 19:48:51 -0800

    fix: too much padding on mobile version. Added breakpoints for smaller devices

--- src/components/projectCards.tsx
+++ src/components/projectCards.tsx
@@ -54,11 +54,11 @@ const projects = [
 
 const ProjectCards = () => {
   return (
-    <div className='w-full px-8 py-16'>
+    <div className='w-full sm:px-8 py-16 px-1'>
       <h2 className='text-3xl font-bold mb-8 text-center text-white'>
         My Projects
       </h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-6'>
         {projects.map((project, index) => (
           <Card
             key={index}
